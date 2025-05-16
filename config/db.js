09cf186f418708c2816e919ad21632e4a05385e7
hayeon7898 <hayeon22@korea.ac.kr>
2025-05-16 22:19:56 +0900

    fix: connecting db

--- config/db.js
+++ config/db.js
@@ -15,10 +15,12 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,  // 클라우드 DB 보안 연결 시 필요 (Neon, Supabase 등)
-  },
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false }  // 필요하면 SSL 옵션 추가
 });
 
 module.exports = pool;
