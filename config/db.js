3c69e0b7f51a0bbf80bdb6703da9e1e8e082b078
hayeon7898 <hayeon22@korea.ac.kr>
2026-01-17 14:36:18 +0900

    fix: config/db.js (error)

--- config/db.js
+++ config/db.js
@@ -14,8 +14,6 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const { Pool } = require("pg");
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
